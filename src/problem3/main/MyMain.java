7acce6fce0abbb315d0fdac5a092dc17c1cc8a6a
kvcs <44372851+Khushi-Verma@users.noreply.github.com>
2020-03-28 08:24:03 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -8,4 +8,14 @@ package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+   public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("khushi",1);
+        obj.insert("jalanki",4);
+        obj.insert("vaibhav",3);
+        obj.insert("riya",2);
+        obj.insert("sameeksha",5);
+        obj.display();
+    }
+  
 }
