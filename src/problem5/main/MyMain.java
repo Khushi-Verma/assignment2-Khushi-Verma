19d9ddf3a20e3fa7ed8889eef33bd406dff8c7a3
kvcs <44372851+Khushi-Verma@users.noreply.github.com>
2020-03-28 08:34:43 +0530

    Update MyMain.java

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -7,4 +7,16 @@
 package problem5.main;
 //executable class
 public class MyMain {
+  public static void main(String[] args) {
+        MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(1,3,0);
+        obj.enqueue(2,6,1);
+        obj.enqueue(3,6,4);
+        obj.enqueue(4,2,1);
+        obj.enqueue(5,1,2);
+        obj.enqueue(6,0,0);
+        obj.removeBackLog();
+        obj.display();
+
+    }
 }
